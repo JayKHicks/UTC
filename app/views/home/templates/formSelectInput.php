0650cb0bae55f40330c54d98c2b9a33a45da5669
U-US\TreWhite <TreWhite@GMTI-ORF-105663.us.ad.gannett.com>
2015-04-22 13:10:10 -0400

    UCT-initial - adding attributes to form fields

--- app/views/home/templates/formSelectInput.php
+++ app/views/home/templates/formSelectInput.php
@@ -1,6 +1,6 @@
 <div class="col-sm-<?php echo $item['size'] ?>">
 	<label class="control-label"><?php echo $item['label'] ?></label>
-	<select class="form-control <?php echo $item['classname'] ?>">
+	<select class="form-control <?php echo $item['classname'] ?>" <?php if(isset($item['attr'])) echo $item['attr'] ?>>
 		<option val="">--</option>
 	</select>
 </div>
